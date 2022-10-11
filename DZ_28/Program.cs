19a72ba40fd5405c806b3b1375763c98e57d6e2f
Andrey <galan121@yandex.ru>
2022-10-11 23:12:16 +0300

    DZ_28

---
+++ DZ_28/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение 
+чисел от 1 до N. 4 -> 24  5 -> 120 */
+
+
+int num = int.Parse(Console.ReadLine());
+  int result = addition(num);  
+  Console.WriteLine(result);  
+  int addition(int number)
+   {     
+    int count = 1;        
+    for (int i = 1; i <= number; i++)  
+ {         
+    count *= i;    
+ }      
+    return count;
+ }
