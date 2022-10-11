73451cbdfb914a9fe7de452dc796a166b58781f0
Andrey <galan121@yandex.ru>
2022-10-11 23:22:07 +0300

    DZ_30

---
+++ DZ_30/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+ заполненный нулями и единицами в случайном порядке. 1,0,1,1,0,1,0,0 */
+int[] arr = new int[8];
+for (int i = 0; i < arr.Length; i++)       
+{            
+arr[i] = new Random().Next(0, 2);   
+if (i == arr.Length - 1) 
+    Console.Write($"{arr[i]}. ");             
+else Console.Write($"{arr[i]}, ");       
+ }
