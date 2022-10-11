77c8ad4cfdc5bb4b925f3546f6d376fb7d8b63f2
Andrey <galan121@yandex.ru>
2022-10-11 06:09:57 +0300

    DZ_10

---
+++ DZ_10/Program.cs
@@ -0,0 +1,13 @@
+﻿/* на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Write("Введите трёхзначное число N:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n<100|n>999)
+{
+Console.WriteLine("Вы ввели недопустимое число, повторите ввод: ");
+ n = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Последняя цифра числа: " + Convert.ToString(n%10));
