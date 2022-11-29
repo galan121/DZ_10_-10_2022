40fbdca2afb1ce16b650570504cf023d693a66b3
Andrey <galan121@yandex.ru>
2022-11-29 20:45:58 +0300

    DZ_54

--- DZ_13/Program.cs
+++ DZ_13/Program.cs
@@ -7,7 +7,7 @@ Console.Write("Введите трёхзначное число N:  ");
 int n = Convert.ToInt32(Console.ReadLine());
 while (n<100|n>999)
 {
-Console.WriteLine("Вы ввели недопустимое число, повторите ввод: ");
+Console.WriteLine("В данном числе нет третьей цифры, повторите ввод: ");
  n = Convert.ToInt32(Console.ReadLine());
 }
 Console.Write("Последняя цифра числа: " + Convert.ToString(n%10));
